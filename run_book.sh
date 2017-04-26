c3cf4df187e60619f86e905f4c7c4f2ac726969e
rucka <rucka@tiscalinet.it>
2017-04-26 14:49:18 +0200

    wip

--- run_book.sh
+++ run_book.sh
@@ -1,11 +1,8 @@
 if [ ! "$(docker ps -q -f name=tensor)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=tensor)" ]; then
-        # cleanup
         docker rm tensor
     fi
-    # run your container
   docker run --name tensor -it -v $HOME/Projects/NeuralNetworkPlayground/notebook:/book:rw -p 8888:8888 -p 6006:6006 -w /book tensorflow/tensorflow sh -c "/run_jupyter.sh --allow-root & tensorboard --logdir=/tmp/tensorflow/mnist/logs/mnist_with_summaries"
 fi
 #docker run --name tensor -it -v $HOME/Projects/NeuralNetworkPlayground/notebook:/book:rw -p 8888:8888 -p 6006:6006 -w /book tensorflow/tensorflow sh -c "/run_jupyter.sh --allow-root & tensorboard --logdir=`pwd`"
-
 #docker run --name tensor -it -v $HOME/Projects/NeuralNetworkPlayground/notebook:/book:rw -p 8888:8888 -p 6006:6006 -w /book tensorflow/tensorflow sh -c "/run_jupyter.sh --allow-root & tensorboard --logdir=/tmp/tensorflow/mnist/logs/mnist_with_summaries"
