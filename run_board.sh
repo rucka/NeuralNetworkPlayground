5d25b6dcb2f140943d9a3728fefa347eed880b8c
rucka <rucka@tiscalinet.it>
2017-04-30 10:00:31 +0200

    add tensorflow/models submodule

--- run_board.sh
+++ run_board.sh
@@ -2,5 +2,5 @@ if [ ! "$(docker ps -q -f name=board)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=board)" ]; then
         docker rm board
     fi
-  docker run --name board -it -v $(pwd)/logs:/logs -p 6006:6006 -w /book tensorflow/tensorflow sh -c "tensorboard --logdir=/logs/$1"
+  docker run --name board -it -v $(pwd)/models:models -v $(pwd)/logs:/logs -p 6006:6006 -w /book tensorflow/tensorflow sh -c "tensorboard --logdir=/logs/$1"
 fi
