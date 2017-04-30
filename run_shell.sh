ccb44d5a39cdc0d27c67b0052770ac10401bc812
rucka <rucka@tiscalinet.it>
2017-04-30 10:17:07 +0200

    added run shell script

---
+++ run_shell.sh
@@ -0,0 +1,6 @@
+if [ ! "$(docker ps -q -f name=tfshell)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=tfshell)" ]; then
+        docker rm tfshell
+    fi
+    docker run --name tfshell -it -v $(pwd)/models:/models -v $(pwd)/code:/code -v $(pwd)/logs:/logs -v $(pwd)/notebook:/book -p 8888:8888 -w /code tensorflow/tensorflow /bin/bash
+fi
