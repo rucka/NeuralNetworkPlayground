e5177c2cc84d0bc4c7a0d54b9352db2cb1d7601c
rucka <rucka@tiscalinet.it>
2017-05-03 08:20:15 +0200

    added rm flag to docker run

--- run_book.sh
+++ run_book.sh
@@ -2,5 +2,5 @@ if [ ! "$(docker ps -q -f name=tensor)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=tensor)" ]; then
         docker rm tensor
     fi
-    docker run --name tensor -it -v $(pwd)/logs:/logs -v $(pwd)/notebook:/book -p 8888:8888 -w /book tensorflow/tensorflow sh -c "/run_jupyter.sh --allow-root"
+    docker run --rm --name tensor -it -v $(pwd)/logs:/logs -v $(pwd)/notebook:/book -p 8888:8888 -w /book tensorflow/tensorflow sh -c "/run_jupyter.sh --allow-root"
 fi
