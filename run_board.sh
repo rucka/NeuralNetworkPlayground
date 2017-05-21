ecd2e963d32021760804440dee22ccb6792031c1
rucka <rucka@tiscalinet.it>
2017-05-21 21:43:16 +0200

    refactoring run scripts

--- run_board.sh
+++ run_board.sh
@@ -2,5 +2,5 @@ if [ ! "$(docker ps -q -f name=board)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=board)" ]; then
         docker rm board
     fi
-  docker run --rm --name board -it -v $(pwd)/data:data -v $(pwd)/models:models -v $(pwd)/logs:/logs -p 6006:6006 -w /book tensorflow/tensorflow sh -c "tensorboard --logdir=/logs/$1"
+  docker run --rm --name board -it -v $(pwd)/tmp:/tmp -v $(pwd)/data:/data -v $(pwd)/models:/models -v $(pwd)/logs:/logs -p 6006:6006 -w /book tensorflow/tensorflow sh -c "tensorboard --logdir=$1"
 fi
