c2729d50ac3a06f416aac358726526277d235160
rucka <rucka@tiscalinet.it>
2017-05-21 12:28:12 +0200

    added tmp dir to run_shell script

--- run_shell.sh
+++ run_shell.sh
@@ -2,5 +2,5 @@ if [ ! "$(docker ps -q -f name=tfshell)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=tfshell)" ]; then
         docker rm tfshell
     fi
-    docker run --rm --name tfshell -it -v $(pwd)/data:/data -v $(pwd)/models:/models -v $(pwd)/code:/code -v $(pwd)/logs:/logs -v $(pwd)/notebook:/book -p 8888:8888 -w /code tensorflow/tensorflow /bin/bash
+    docker run --rm --name tfshell -it -v $(pwd)/tmp:/tmp -v $(pwd)/data:/data -v $(pwd)/models:/models -v $(pwd)/code:/code -v $(pwd)/logs:/logs -v $(pwd)/notebook:/book -p 8888:8888 -w /code tensorflow/tensorflow /bin/bash
 fi
